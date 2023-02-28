ca40bc5a6a218f5005285cbf5f1944592dffa5b9
AdaN6 <a_disney_da@hotmail.com>
2023-02-28 20:03:12 +0100

    unit test - basic functions - import chai assertion library

--- src/letter-count.test.js
+++ src/letter-count.test.js
@@ -1,13 +1,16 @@
+import {expect} from 'chai';
 
-
-import { getLettercount } from './letter-count';
+import { getLetterCount } from './letter-count';
 
 // mocha keyword describe
 
 describe('getLetterCount - basic functionality', () => {
     // it is denote individual test, need two arguments
     it('returns an empty object when passed an empty string', () => {
-
+            const expected = {};
+            const actual = getLetterCount('');
+            // deep keyword is to comparing objects
+            expect(actual).to.deep.equal(expected);
     })
 })
 
