5833ced592d8abf51e287b5625b56ebcdcbbe8f1
AdaN6 <a_disney_da@hotmail.com>
2023-02-28 19:58:03 +0100

    unit test - mocha describe

---
+++ src/letter-count.test.js
@@ -0,0 +1,13 @@
+
+
+import { getLettercount } from './letter-count';
+
+// mocha keyword describe
+
+describe('getLetterCount - basic functionality', () => {
+    // it is denote individual test, need two arguments
+    it('returns an empty object when passed an empty string', () => {
+
+    })
+})
+
