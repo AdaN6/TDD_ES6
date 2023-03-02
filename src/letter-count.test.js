9a649ff625e5854b32918e0218a8c74d589c2865
AdaN6 <a_disney_da@hotmail.com>
2023-03-02 18:43:52 +0100

    unit test: wrote 2nd failing test

--- src/letter-count.test.js
+++ src/letter-count.test.js
@@ -12,13 +12,19 @@ describe('getLetterCount - basic functionality', () => {
             // deep keyword is to comparing objects
             expect(actual).to.deep.equal(expected);
     })
+    // ---  1
+    // then run the first test --> npx mocha "src/**/*.test.js" --recursive --require @babel/register
+    // it should be failed, just to have the first failing test, just to make sure wont give us a false positive
+    
+    
+    // --- 2
+    // in order not to rewrite a big long command in terminal to run a test need to change the test from script. 
+    // changed from  "echo \"Error: no test specified\" && exit 1" to "npx mocha \"src/**/*.test.js\" --recursive --require @babel/register"
+    // in Terminal -> npm run test
+
+    it ('return the correct letter count for a word with only one of each letter ',() => {
+        const expected = { c: 1, a:1, t:1};
+        const actual = getLetterCount('cat');
+        expect(actual).to.deep.equal(expected)
+    })
 })
-// ---  1
-// then run the first test --> npx mocha "src/**/*.test.js" --recursive --require @babel/register
-// it should be failed, just to have the first failing test, just to make sure wont give us a false positive
-
-
-// --- 2
-// in order not to rewrite a big long command in terminal to run a test need to change the test from script. 
-// changed from  "echo \"Error: no test specified\" && exit 1" to "npx mocha \"src/**/*.test.js\" --recursive --require @babel/register"
-// in Terminal -> npm run test
