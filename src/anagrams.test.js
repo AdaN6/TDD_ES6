43a538e3aa402142845ab947a3d57e3320100e39
AdaN6 <a_disney_da@hotmail.com>
2023-03-09 19:40:46 +0100

    created anagrams.js and test files

---
+++ src/anagrams.test.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+
+import { isAnagrams } from "./anagrams";
+
+// isAnagrams(string1 , string2)
+//"listen"-"silent"
+//spaces should be ignored
+//case should be ignored
